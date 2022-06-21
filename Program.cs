a6e2c0c100e8137836b86d2f62b4f8a1822079b3
a-kotynski <94733884+a-kotynski@users.noreply.github.com>
2022-06-21 10:27:10 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice21June3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Width: ");
+            int width = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Height: ");
+            int height = Convert.ToInt32(Console.ReadLine());
+
+            if (width > height)
+            {
+                Console.WriteLine("landscape");
+            }
+            else if (height > width)
+            {
+                Console.WriteLine("portrait");
+            }
+        }
+    }
+}
